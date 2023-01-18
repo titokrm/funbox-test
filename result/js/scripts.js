58f7e173acb89481b577f4c0b84eaa729b6365d4
Roman Titok <titokr@gmail.com>
2023-01-18 05:03:33 +0200

    Fix script and styles

--- result/js/scripts.js
+++ result/js/scripts.js
@@ -1,39 +1,36 @@
 document.addEventListener( 'DOMContentLoaded', function() {
   let body = document.querySelector('body');
   let cardOver;
+
   function handleMouseOver(event) {
     const cardClick = event.target.closest('.feed-card__bg');
     if (cardClick && !cardClick.closest('.feed-card').classList.contains('feed-card--disabled')) {
-      console.log('Over card!');
+  //    console.log('Over card!');
       event.target.closest('.feed-card').classList.add('feed-card--hover');
       cardOver = cardClick;
       cardClick.addEventListener('mouseleave', handleMouseLeave);
     }    
   }
+
   function handleMouseLeave(event) {
     if (event.target.classList.contains('feed-card__bg')) {
-      console.log('Out card!');
+//      console.log('Out card!');
       event.target.closest('.feed-card').classList.remove('feed-card--hover');
       cardOver.removeEventListener('mouseleave', handleMouseLeave);
     }
-//    const cardClick = event.target.closest('.feed-card__bg');
-//    if (cardClick) {
-//      console.log('Out!');
-//      event.target.closest('.feed-card').classList.remove('feed-card--hover');
-//    }
-    
   }
+
   function handleActiveMouseLeave(event) {
     cardOver.removeEventListener('mouseleave', handleActiveMouseLeave);
     body.addEventListener('mouseover', handleMouseOver);  
   }
-  body.addEventListener('mouseover', handleMouseOver);
 
+  body.addEventListener('mouseover', handleMouseOver);
   body.addEventListener('click', (event) => {
     const cardClick = event.target.closest('.feed-card__bg');
     const linkClick = event.target.closest('.feed-card__description-text a');
     if (cardClick && !cardClick.closest('.feed-card').classList.contains('feed-card--disabled')) {
-      console.log('Click card!');
+//      console.log('Click card!');
       let card = event.target.closest('.feed-card');
       card.classList.remove('feed-card--hover');
       card.classList.toggle('feed-card--active');
@@ -46,7 +43,6 @@ document.addEventListener( 'DOMContentLoaded', function() {
       let card = event.target.closest('.feed-card');
       card.classList.add('feed-card--active');
     }
-
   });
 
 });
